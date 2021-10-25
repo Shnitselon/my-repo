94268eab5124419932d0fa3a36496ee62ad09bb6
Steve <20701198+Shnitselon@users.noreply.github.com>
2021-10-25 16:20:43 +0300

    Add files via upload

---
+++ create-admin-user.ps1
@@ -0,0 +1,8 @@
+﻿$username = "myadmin"
+$password = ConvertTo-SecureString "Password123" -AsPlainText -Force
+
+$group = "Administrators"
+
+New-LocalUser -Name $username -Description "My local admin user." -Password $password
+
+Add-LocalGroupMember -Group $group -Member $username
